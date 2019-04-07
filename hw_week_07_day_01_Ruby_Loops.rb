941a8c4f49ee308c7c78e3a0df683e7effa3819f
bob <bob@>
2019-04-07 20:11:33 +0300

    solve Q1 and Q2

--- hw_week_07_day_01_Ruby_Loops.rb
+++ hw_week_07_day_01_Ruby_Loops.rb
@@ -1,6 +1,16 @@
 # 1- Easy Going
+#loop that will log the numbers 1 through 20.
+(1..20).each do |i|
+puts i
+end 
 
 # 2- Get Even
+#loop that will log only the even numbers in 0 through 200.
+(0..200).each do |i|
+    if i%2==0
+        puts i
+    end
+end
 
 # 3- Wild Wild Life
 
