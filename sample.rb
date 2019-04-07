07382c8cd810a6733e78d50d4a521a40f5c87059
alhanoufoth33 <alhanoufoth33@gmail.com>
2019-04-07 23:53:49 +0300

    Hw day1 W7

---
+++ sample.rb
@@ -0,0 +1,139 @@
+# # ## Easy Going
+# # 1. Write a for loop that will log the numbers 1 through 20.
+
+(1..20).to_a.each do |num|
+
+    puts num.to_i
+
+end
+
+
+# # ## Get Even
+# # 1. Write a for loop that will log only the even numbers in 0 through 200.
+
+
+(1..200).to_a.each do |num| 
+    if num.to_i % 2 == 0
+        puts num
+    end
+end
+
+
+# # ## Wild Wild Life
+# # Use the following arrays to answer the questions below (name, species ,age, hometown):
+# # You should be modifying the elements by accessing them. It is up to you which methods to use. 
+
+# # ```ruby
+wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+sharky = ["Sharky", "shark", 20, "Left Coast"]
+plantee = ["Plantee", "plant",  5000 , "Mordor"]
+porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+# # ```
+# # 1. Plantee just had her birthday; change Plantee's array to reflect her being a year older.
+plantee[2] = plantee[2] + 1
+p plantee
+
+# # 2. Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
+wolfy[3] = "Gotham City"
+p wolfy
+# # 3. Give D'Art a second hometown by adding "Hawkins"
+dart <<  "Hawkins"
+p dart
+# # 4. Porgee decides that Wolfy can't be named "Wolfy" anymore. Replace "Wolfy" from the `wolfy` array with "Gameboy".
+wolfy[0] = "Gameboy"
+p wolfy
+
+
+# # ## Yell at the Ninja Turtles
+# # 1. Create an array with the members of the ninja turtles (Donatello, Leonardo, Raphael, Michaelangelo)
+array = ["Donatello, Leonardo, Raphael, Michaelangelo"]
+# # 2. Capitalize each one of them and print out the result.
+array.each do |ninja_turtles|
+    puts ninja_turtles.upcase
+end 
+
+
+# # ## More Arrays
+
+# # ```ruby
+ favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Imitation of Life', 'Snatch', 'Fast and Furious']
+# # ```
+    
+# #  1. Sort the array
+puts favMovies.sort
+
+# #  1. Reverse it 
+puts favMovies.reverse
+#  1. Use the method `pop`
+puts favMovies.pop
+#  1. `push` "Guardians of the Galaxy" 
+favMovies.push "Guardians of the Galaxy"
+p favMovies
+# #  1. Remove `Jaws`
+favMovies.shift
+p favMovies
+# #  1. Add this array ```["Spiderman","Thor","Ant-Man"]``` to `favMovies`
+favMovies = favMovies << ["Spiderman","Thor","Ant-Man"]
+p favMovies 
+#  1. Check if the array includes `"Imitation of Life"` if it does print "already here", else add it to the array.
+
+ if favMovies.include?("Imitation of Life")
+p 'already here'
+else
+    favMovies.push("Imitation of Life")
+end
+
+ p favMovies
+
+# #  1. Print your final results to the console
+# p favMovies
+# ## Where is Waldo
+
+# * With the following multi-dimensional array
+
+# ```ruby
+ whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
+                    ["Lucinda", "Jacc", "Neff", "Snoop"],
+                    ["Petunia", ["Baked Goods", "Waldo"]]];
+# ```
+# * Remove Eggbert 
+whereIsWaldo.delete_at(1)
+p whereIsWaldo
+# * Change "Neff" to "No One"
+whereIsWaldo[1][2] = "No One"
+p whereIsWaldo
+# * Access and print "Waldo"
+p whereIsWaldo[2][1][1]
+
+
+# ## Excited Kitten
+# 1. Write code that logs "Love me, pet me! HSSSSSS!" 20 times.
+num = 1
+while num <=20 do
+    p "Love me, pet me! HSSSSSS!"
+    num = num +1
+end
+# 2. For every **even** number in your loop, log "...human...why you taking pictures of me?...".
+num =1
+while num <=20 do
+    p "Love me, pet me! HSSSSSS!"
+    if num %2 == 0 
+        p "...human...why you taking pictures of me?..."
+    end
+    num = num+1
+end
+
+# ## A New Friend
+
+# 1. Create a loop that prints `Hello` forever unless the user inputs `Goodbye`
+  
+puts "Hello "
+
+name = gets.chomp
+
+until name == "Goodbye"
+     
+        puts "Hello"
+        name = gets.chomp
+end 
