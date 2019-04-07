4e59347c884ca490f28c3bb2fd72a53c23ed6466
bob <bob@>
2019-04-07 20:20:25 +0300

    solve Q3

--- hw_week_07_day_01_Ruby_Loops.rb
+++ hw_week_07_day_01_Ruby_Loops.rb
@@ -13,7 +13,27 @@ end
 end
 
 # 3- Wild Wild Life
+wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+sharky = ["Sharky", "shark", 20, "Left Coast"]
+plantee = ["Plantee", "plant",  5000 , "Mordor"]
+porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
 
+#change Plantee's array to reflect her being a year older.
+plantee[2]=plantee[2]+1
+
+#Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
+wolfy[3]="Gotham City"
+
+#Give D'Art a second hometown by adding "Hawkins"
+dart[3]=dart[3]+" ,Hawkins"
+
+#Replace "Wolfy" from the wolfy array with "Gameboy".
+wolfy.shift()
+wolfy.unshift("Gameboy")
+ # another way
+wolfy[0]="Gameboy"
+puts wolfy
 # 4- Yell at the Ninja Turtles
 
 # 5- More Arrays
