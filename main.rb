7d654504d5af0a788468d75caaa056228830e60a
rehabsaadoun <YOUR GITHUB EMAIL>
2019-04-08 06:28:30 +0300

    hw7

---
+++ main.rb
@@ -0,0 +1,76 @@
+## Easy Going
+(1..2).to_a.each do |num|
+    puts num.to_i
+end
+
+## Get Even
+(1..20.to).to_a.each do |num|
+    if num.to.i %2 ==0
+        puts num.to_i
+
+    end
+    ## Wild Wild Life
+    wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+sharky = ["Sharky", "shark", 20, "Left Coast"]
+plantee = ["Plantee", "plant",  5000 , "Mordor"]
+porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+
+
+    Plantee [2]= plantee [2] +1
+    puts plantee
+
+    Wolfy[3]="Gotham City"
+    puts  Wolfy
+
+    dart << "Hawkins"
+    puts  dart
+
+    ## Yell at the Ninja Turtles
+
+    ['Donatello', 'Leonardo',' Raphael', 'Michaelangelo'].each do |the Ninja Turtles| 
+        print the Ninja Turtles.capitalize  
+     end
+## More Arrays
+
+
+favMovies = ['Jaws', 'The Fellowship of the Ring', 
+'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 
+'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 
+'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 
+'Oculus', 'Seven', 'Black Panther',
+ 'Imitation of Life', 'Snatch', 'Fast and Furious'];
+ favMovies.push ( "Guardians of the Galaxy")
+
+ favMovies.delete ('Jaws')
+ favMovies.add ["Spiderman","Thor","Ant-Man"]
+ puts "adding #{Spiderman}+{Thor}={Ant-Man}"
+    end
+
+## Where is Waldo
+whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
+                    ["Lucinda", "Jacc", "Neff", "Snoop"],
+                    ["Petunia", ["Baked Goods", "Waldo"]]];
+
+                    whereIsWaldo.delete ('Eggbert ')
+end
+                    Neff[3]="No One"
+                    puts  Neff
+end
+print ["Waldo"]
+end
+
+## Excited Kitten
+20.times { print "Love me, pet me! HSSSSSS! "}
+
+
+     ## A New Friend
+     loop do "Hello"
+        New Friend = gets.chomp
+    
+    break if New Friend == "Goodbye"
+    
+    end
+
+
